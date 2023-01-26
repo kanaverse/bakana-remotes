b9500dfb1cea1a7b3883a9b105ec2025c1177f32
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-26 15:18:36 -0800

    Test for correct loading of a ScranMatrix.

--- src/ArtifactDB.js
+++ src/ArtifactDB.js
@@ -2,6 +2,7 @@ import * as adb from "artifactdb";
 import * as bakana from "bakana";
 import * as scran from "scran.js";
 import * as bioc from "bioconductor";
+//import * as chihaya from "chihaya";
 
 function detect_csv_compression(details) {
     if ("csv_data_frame" in details) {
@@ -22,7 +23,12 @@ function detect_csv_compression(details) {
  * Non-atomic columns are currently skipped, and factors/dates are treated as string arrays.
  *
  * @param {string} baseUrl - Base URL of the ArtifactDB instance.
- * @param {string} id - Identifier for the DataFrame.
+ * @param {string} id - Identifier for the data frame.
+ * @param {object} [options={}] - Optional parameters.
+ * @param {?function} [options.getFun=null] - Function that accepts a URL string and performs a GET to return a Response object,
+ * see [`getFileMetadata`](https://artifactdb.github.io/artifactdb.js/global.html#getFileMetadata) for details.
+ * @param {?function} [options.downloadFun=null] - Function that accepts a URL string and downloads the resource, returning either a file path (string) or an ArrayBuffer containing the file contents;
+ * see [`getFile`](https://artifactdb.github.io/artifactdb.js/global.html#getFile) for details.
  *
  * @return {external:DataFrame} The DataFrame, absent any "other" columns.
  */
@@ -160,4 +166,77 @@ export async function loadDataFrame(baseUrl, id, { getFun = null, downloadFun = 
     }
 
     return new bioc.DataFrame(recolumns, { rowNames: row_names, columnOrder: reheaders });
+}
+
+/**
+ * Load a sparse {@linkplain external:ScranMatrix} given an ArtifactDB ID.
+ * This currently supports loading from `hdf5_dense_array`, `hdf5_sparse_matrix` and `hdf5_delayed_array` instances.
+ *
+ * @param {string} baseUrl - Base URL of the ArtifactDB instance.
+ * @param {string} id - Identifier for the data frame.
+ * @param {object} [options={}] - Optional parameters.
+ * @param {?function} [options.getFun=null] - Function that accepts a URL string and performs a GET to return a Response object,
+ * see [`getFileMetadata`](https://artifactdb.github.io/artifactdb.js/global.html#getFileMetadata) for details.
+ * @param {?function} [options.downloadFun=null] - Function that accepts a URL string and downloads the resource, returning either a file path (string) or an ArrayBuffer containing the file contents;
+ * see [`getFile`](https://artifactdb.github.io/artifactdb.js/global.html#getFile) for details.
+ * @param {boolean} [options.forceInteger=true] - Whether to force an integer representation for the matrix data.
+ * This saves space when floating-point is unnecessary. 
+ * @param {boolean} [options.layered=true] - Whether to use a layered representation for the sparse matrix.
+ * This saves space but adds complexity in downstream analysis by permuting the rows.
+ *
+ * @return {object} An object containing:
+ * - `matrix`, a {@linkplain external:ScranMatrix} containing the contents of the matrix.
+ * - `row_ids`, an Int32Array containing the row identities, possibly permuted when `layered = true`.
+ */
+export async function loadScranMatrix(baseUrl, id, { getFun = null, downloadFun = null, forceInteger = true, layered = true } = {} ) {
+    let details = await adb.getFileMetadata(baseUrl, id, { getFun });
+    let unpacked = adb.unpackId(id);
+    let content = await adb.getFile(baseUrl, adb.packId(unpacked.project, details.path, unpacked.version), { getFun, downloadFun });
+
+    let schema = details["$schema"]
+    let is_dense = (schema.startsWith("hdf5_dense_array/") || "hdf5_dense_array" in details);
+    let is_sparse = (schema.startsWith("hdf5_sparse_matrix/") || "hdf5_sparse_matrix" in details);
+    let output = {};
+
+    let mock_consecutive_ids = NR => {
+        let dummy = new Int32Array(NR);
+        dummy.forEach((x, i) => { dummy[i] = i; });
+        return dummy;
+    };
+
+    if (is_dense || is_sparse) {
+        let group = (is_dense ? details.hdf5_dense_array.dataset : details.hdf5_sparse_matrix.group);
+        let finfo = scran.realizeFile(content);
+        try {
+            output = scran.initializeSparseMatrixFromHDF5(finfo.path, group, { forceInteger: forceInteger, layered: layered });
+            if (output.row_ids == null) {
+                output.row_ids = mock_consecutive_ids(output.matrix.numberOfRows());
+            }
+        } finally {
+            finfo.flush();
+        }
+
+//    } else if (schema.startsWith("hdf5_delayed_array/") || "hdf5_delayed_array" in details) {
+//        let finfo = scran.realizeFile(contents);
+//        try {
+//            output.matrix = await chihaya.load(finfo.path, details.hdf5_delayed_array.group);
+//            if (output.row_ids == null) {
+//                output.row_ids = mock_consecutive_ids(output.matrix.numberOfRows());
+//            }
+//        } finally {
+//            finfo.flush();
+//        }
+
+    } else {
+        throw new Error("only supporting 'hdf5_dense_matrix', 'hdf5_sparse_matrix' or 'hdf5_delayed_array' objects right now");
+    }
+
+    let expected_dims = details["array"]["dimensions"];
+    let loaded = output.matrix;
+    if (loaded.numberOfRows() != expected_dims[0] || loaded.numberOfColumns() != expected_dims[1]) {
+        scran.free(loaded);
+        throw new Error("dimensions of the matrix is not consistent with its metadata");
+    }
+
+    return output;
 }
--- tests/ArtifactDB.test.js
+++ tests/ArtifactDB.test.js
@@ -1,5 +1,9 @@
 import * as remotes from "../src/index.js";
 import * as utils from "./utils.js";
+import * as bakana from "bakana";
+
+beforeAll(utils.initializeAll);
+afterAll(async () => await bakana.terminate());
 
 let test_url = "https://collaboratordb.aaron-lun.workers.dev";
 let getter = async url => new Response(await utils.downloader(url));
@@ -17,4 +21,38 @@ test("Loading a CSV DataFrame works as expected", async () => {
     let df2 = await remotes.loadDataFrame(test_url, "dssc-test_basic-2023:my_first_sce/rowdata/simple.csv.gz@2023-01-19", options);
     expect(df2.column("featureType").length).toBe(df2.numberOfRows());
     expect(df2.rowNames().length).toBe(df2.numberOfRows());
+})
+
+test("Loading a ScranMatrix works as expected", async () => {
+    let options = { getFun: getter, downloadFun: utils.downloader };
+
+    // Loading from a sparse matrix.
+    {
+        let test_id = "dssc-test_basic-2023:my_first_sce/assay-1/matrix.h5@2023-01-19";
+
+        let mat = await remotes.loadScranMatrix(test_url, test_id, options);
+        expect(mat.matrix.numberOfRows()).toBeGreaterThan(0);
+        expect(mat.matrix.numberOfColumns()).toBeGreaterThan(0);
+        expect(mat.matrix.isSparse()).toBe(true);
+        expect(mat.row_ids.length).toEqual(mat.matrix.numberOfRows());
+
+        // Trying without any layering.
+        let unlayered = await remotes.loadScranMatrix(test_url, test_id, { ...options, layered: false });
+        expect(unlayered.matrix.numberOfRows()).toEqual(mat.matrix.numberOfRows());
+        expect(unlayered.matrix.numberOfColumns()).toEqual(mat.matrix.numberOfColumns());
+        expect(unlayered.row_ids.length).toEqual(unlayered.matrix.numberOfRows());
+
+        let mcol = mat.matrix.column(0);
+        let ucol = unlayered.matrix.column(0);
+        expect(Array.from(mat.row_ids).map(i => ucol[i])).toEqual(Array.from(mcol));
+    }
+
+    // Loading a sparse matrix from a dense matrix.
+    {
+        let mat = await remotes.loadScranMatrix(test_url, "dssc-test_airway-2023:airway/assay-1/array.h5@2023-01-19", options);
+        expect(mat.matrix.numberOfRows()).toBeGreaterThan(0);
+        expect(mat.matrix.numberOfColumns()).toBeGreaterThan(0);
+        expect(mat.matrix.isSparse()).toBe(true);
+        expect(mat.row_ids.length).toEqual(mat.matrix.numberOfRows());
+    }
 })
